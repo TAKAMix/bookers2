7804b48b5ad9c946da378fb99b354a4f39e8b8d9
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-07-21 12:34:01 +0000

    [add]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,34 @@ class BooksController < ApplicationController
   
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    book.user_id = current_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
 
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    @book.save
     
-    redirect_to books_path
+    redirect_to book_path(@book.id)
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def show
-    @user = User.find(params[:id])
-    
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:show, :edit, :index, :update]
   resources :users, only: [:show, :edit, :index, :update]
   get "/home/about" => "homes#about", as: "about"
   
   post 'books' => 'books#create'
+  
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
